0a043d9e38391edb98bdbcfa8c3a4666b11a5a07
Vijay Radha <polymorphic.technologies@gmail.com>
2017-02-18 18:44:26 +0000

    Created outline InputReader

--- src/main/java/uk/co/gumtree/addressbook/service/InputReader.java
+++ src/main/java/uk/co/gumtree/addressbook/service/InputReader.java
@@ -1,4 +1,7 @@
 package uk.co.gumtree.addressbook.service;
 
 public class InputReader {
+    public void read(String filePath) {
+
+    }
 }
--- src/test/java/uk/co/gumtree/addressbook/service/InputReaderTest.java
+++ src/test/java/uk/co/gumtree/addressbook/service/InputReaderTest.java
@@ -2,6 +2,7 @@ package uk.co.gumtree.addressbook.service;
 
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class InputReaderTest {
     private InputReader underTest;
@@ -9,6 +10,12 @@ public class InputReaderTest {
     @Before
     public void setUp() throws Exception {
         underTest = new InputReader();
+    }
+
+    @Test
+    public void itShouldReadInputFile() {
+        String filePath = getClass().getResource("/addressBookValid").getPath();
+        underTest.read(filePath);
     }
 
 }
