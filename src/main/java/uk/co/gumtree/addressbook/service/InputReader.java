384d0ccbe84faafacfb5eb6e944617d499e0c73a
Vijay Radha <polymorphic.technologies@gmail.com>
2017-02-18 21:37:24 +0000

    Implemented InputReader to populate AddressBook

--- src/main/java/uk/co/gumtree/addressbook/service/InputReader.java
+++ src/main/java/uk/co/gumtree/addressbook/service/InputReader.java
@@ -1,17 +1,31 @@
 package uk.co.gumtree.addressbook.service;
 
 import uk.co.gumtree.addressbook.bean.AddressBook;
+import uk.co.gumtree.addressbook.bean.Contact;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.text.ParseException;
+import java.util.Objects;
 
 public class InputReader {
 
     public AddressBook read(String filePath) throws IOException {
-        Files.lines(Paths.get(filePath)).forEach(System.out::println);
-        return new AddressBook();
+        AddressBook addressBook = new AddressBook();
+        Files.lines(Paths.get(filePath))
+                .map(s -> {
+                            try {
+                                return new Contact(s);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                            return null;
+                        }
+                    )
+                .filter(Objects::nonNull)
+                .forEach(e -> addressBook.addContact(e));
+        return addressBook;
     }
 
 }
--- src/test/java/uk/co/gumtree/addressbook/service/InputReaderTest.java
+++ src/test/java/uk/co/gumtree/addressbook/service/InputReaderTest.java
@@ -2,9 +2,13 @@ package uk.co.gumtree.addressbook.service;
 
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import uk.co.gumtree.addressbook.bean.AddressBook;
 
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class InputReaderTest {
@@ -16,10 +20,11 @@ public class InputReaderTest {
     }
 
     @Test
-    public void itShouldReturnAddressBookForGivenInputFile() {
+    public void itShouldReturnAddressBookForGivenInputFile() throws IOException {
         String filePath = getClass().getResource("/addressBookValid").getPath();
         AddressBook addressBook = underTest.read(filePath);
         assertNotNull(addressBook);
+        assertEquals(5, addressBook.getContacts().size());
     }
 
 }
